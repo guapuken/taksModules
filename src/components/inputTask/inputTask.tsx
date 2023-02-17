e4a04847927e7b46bbf9efd996e798922a375878
guapuken <44274367+guapuken@users.noreply.github.com>
2023-02-17 16:10:38 -0600

    se comenzo a trabajar el componente de input task

--- src/components/inputTask/inputTask.tsx
+++ src/components/inputTask/inputTask.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../index.css';
+import './inputTask.scss';
 
 export interface InputTaskProps {
 
 }
 const InputTask = ()=>{
+  const [checked,setChecked] = useState(false)
 
   let inputsStyles = {
     width:'100%',
+    border:'none'
   }
   return (
   <div>
-    <input style={{fontSize:'1.8rem',...inputsStyles}} placeholder='Nombre de la tarea' type="text" />
-    <input style={{fontSize:'1.8rem',...inputsStyles}} placeholder='Descripción de la tarea' type="text" />
+    <p>
+      <input type="checkbox" id="test2" onClick={()=>setChecked(!checked)} checked={checked} />
+      <label htmlFor="test2">Yellow</label>
+    </p>
   </div>
     )
 }
