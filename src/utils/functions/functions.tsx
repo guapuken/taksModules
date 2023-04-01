dcce8f6ea9bd818a7536daf0eda4cbbd938858f5
guapuken <rock_michael1104@hotmail.com>
2023-03-31 18:22:20 -0600

    se agregaron las funciones de cardW y cardH que nos retornan el tamaño de las cards automaticamente dependiendo el tamaño de la pantalla

---
+++ src/utils/functions/functions.tsx
@@ -0,0 +1,17 @@
+import { useWindowSize } from '../widthSize';
+
+// regresa el tamaño ideal que debería de conservar las cards de tareas ------------------------------------
+export function cardW(aside: boolean) {
+	const scrW = useWindowSize().width;
+	return scrW <= 415
+		? scrW / 10 - (aside ? 8 : 3)
+		: scrW <= 834
+		? scrW / 10 / 2 - (aside ? 8 : 3)
+		: scrW / 10 / 4 - (aside ? 8 : 3);
+}
+
+//regresa el tamaño ideal de la altura que debería dde conseervar las cards de tareas------------------------
+export function cardH() {
+	const scrH = useWindowSize().height;
+	return scrH / 10 / 4 - 3;
+}
