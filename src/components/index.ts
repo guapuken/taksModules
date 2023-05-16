bb3934ca256b03108ee601c82afdcd077d9c11c5
guapuken <rock_michael1104@hotmail.com>
2023-05-16 14:11:13 -0600

    se subio a version 0.1.50

--- src/components/index.ts
+++ src/components/index.ts
@@ -96,7 +96,7 @@ export {default as ModalTask} from './modalTareas';
 export {default as ModalTemplates} from './modalTemplates';
 export {default as ModalProjects} from './modalProjects';
 export {default as ModalTeams} from './modalTeams';
-export {default as ModalTaskWithComents} from './modalTasksWithComents';
+export {default as ModalTaskWithComments} from './modalTasksWithComents';
 
 // ---------------------------------------------------------------------------------------------------------
 //      componentes complejos
