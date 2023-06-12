63785711e63539931804c2498b826071ae090f2d
guapuken <rock_michael1104@hotmail.com>
2023-06-12 10:25:32 -0600

    se agrego funcion para ponerl el color de la lettra dependiendo el fondo

--- src/utils/functions/functions.tsx
+++ src/utils/functions/functions.tsx
@@ -59,3 +59,41 @@ export function getWidth(idToSelect: string) {
 
 	return { width, height };
 }
+
+function obtenerColorLetra(colorFondo: any) {
+	/*
+	 * Devuelve el color de letra (blanco o negro) en función del color de fondo proporcionado.
+	 */
+	// Convertir el color de fondo a su equivalente en escala de grises
+	// El cálculo se basa en la fórmula de luminosidad relativa (https://en.wikipedia.org/wiki/Relative_luminance)
+	let r = 0;
+	let g = 0;
+	let b = 0;
+	if (colorFondo.includes('#')) {
+		r = parseInt(colorFondo.slice(1, 3), 16);
+		g = parseInt(colorFondo.slice(3, 5), 16);
+		b = parseInt(colorFondo.slice(5, 7), 16);
+	} else {
+		[r, g, b] = colorFondo;
+	}
+	const luminosidad = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
+
+	// Determinar el color de letra en función de la luminosidad del fondo
+	if (luminosidad > 0.5) {
+		return 'black'; // Fondo claro, usar letra negra
+	} else {
+		return 'white'; // Fondo oscuro, usar letra blanca
+	}
+}
+
+// Ejemplo de uso
+const fondoBlanco = [255, 255, 255];
+const fondoNegro = [0, 0, 0];
+// const fondoBlanco = '#282828';
+// const fondoNegro = '#ffffff';
+
+const colorLetraBlanca = obtenerColorLetra(fondoBlanco);
+const colorLetraNegra = obtenerColorLetra(fondoNegro);
+
+console.log('Color de letra para fondo blanco:', colorLetraBlanca);
+console.log('Color de letra para fondo negro:', colorLetraNegra);
