d71b932a1068adb1c6ff9c581c6d50a532836f72
guapuken <rock_michael1104@hotmail.com>
2023-06-28 18:22:01 -0600

    se repararon los estilos del componente de modalConfirmation

--- src/components/Atoms/importantText/ImportantText.tsx
+++ src/components/Atoms/importantText/ImportantText.tsx
@@ -6,7 +6,7 @@ import './styles/importantText.scss';
 
 const ImportantText = ({ children, modo, weight = 700 }: importantTextProps) => {
 	return (
-		<h2 className={`ctn_ImpText ${modo}`} style={{ fontWeight: weight }}>
+		<h2 className="importantText" theme-config={modo} style={{ fontWeight: weight }}>
 			{children}
 		</h2>
 	);
