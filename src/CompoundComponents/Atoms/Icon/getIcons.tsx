a7bb432ffab8550e07de41cf42c01e06266e95aa
guapuken <rock_michael1104@hotmail.com>
2023-10-30 17:08:25 -0600

    se agrego el componente geticons

--- src/CompoundComponents/Atoms/Icon/getIcons.tsx
+++ src/CompoundComponents/Atoms/Icon/getIcons.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const CloseIcons = (props) => (
+const CloseIcons = (props: any) => (
 	<>
 		<line stroke={props?.fill ?? '#000000'} x1="46.6" y1="46.6" x2="3.4" y2="3.4" />
 		<line stroke={props?.fill ?? '#000000'} x1="46.6" y1="3.4" x2="3.4" y2="46.6" />
 	</>
 );
 
-const GetIcons = ({ children, ...props }) => (
+const GetIcons = ({ children, ...props }: any) => (
 	<div style={{ width: props.size ?? 50, height: props.size ?? 50 }} title={props.title}>
 		<svg viewBox="0 0 50 50" width={props.size ?? 50} height={props.size ?? 50} {...props}>
 			{children}
