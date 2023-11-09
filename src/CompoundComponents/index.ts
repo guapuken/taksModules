c02804103254b66fb8f7408ea4903e7b352e80cb
guapuken <rock_michael1104@hotmail.com>
2023-11-09 17:32:14 -0600

    se exporto el componente de modal desde modalCompound

--- src/CompoundComponents/index.ts
+++ src/CompoundComponents/index.ts
@@ -1,3 +1,4 @@
 export {default as Wizard} from '../CompoundComponents/Wizard';
 export {default as BasicStructure} from '../CompoundComponents/BasicStructure';
 export {default as ButtonCompound} from '../CompoundComponents/Button';
+export {default as Modal} from '../CompoundComponents/Modal/Modal';
