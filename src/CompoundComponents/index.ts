37930a9d5efab49dcef63dd06e6f19c756112a9d
guapuken <rock_michael1104@hotmail.com>
2023-11-14 09:47:01 -0600

    se agrego la exportacion de GetIcons

--- src/CompoundComponents/index.ts
+++ src/CompoundComponents/index.ts
@@ -1,4 +1,5 @@
 export {default as Wizard} from '../CompoundComponents/Wizard';
 export {default as BasicStructure} from '../CompoundComponents/BasicStructure';
 export {default as ButtonCompound} from '../CompoundComponents/Button';
+export {default as GetIcons} from '../CompoundComponents/Icons/GetIcons';
 export {default as Modal} from '../CompoundComponents/Modal/Modal';
